8b980e73af527853c8addb434ddaf13f65ead3b1
sardor-karimov2002 <sardorbekkarimov155@gmail.com>
2022-01-16 15:08:23 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+var galasovay=document.getElementById('galasovay')
+var rec=new webkitSpeechRecognition();
+var divEl=document.createElement('div')
+rec.lang='Uz-uz';
+rec.onstart= function(){
+    console.log("Process started");
+
+}
+
+rec.onresult=function(event){
+    let result=event.results[0][0].transcript
+    recBtn.textContent=result
+     if(result=='1'){
+        document.body.style.background='red'
+    }else if(result=='2'){
+        document.body.style.background='green'
+    }else{
+        document.body.style.background='yellow'
+    }
+    console.log(result);
+
+}
+rec.onend=function(){
+    console.log("Process tugatildi");
+}
+rec.onerror=function(){
+    console.log("xatolik yuz berdi");
+}
+var recBtn=document.createElement('div  ')
+recBtn.textContent='';
+
+galasovay.addEventListener('click', ()=>{
+    
+    rec.start()
+})
+document.body.appendChild(recBtn)
+document.body.appendChild(divEl)
